5613187176fefb9afb9a4b9412810870354dc2c0
ramigabsi10 <rami.gabsi5@gmail.com>
2022-02-06 15:06:11 +0100

    premier projet spring mvc

---
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.sip.ams.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+public class HomeController {
+	
+	@RequestMapping("/information") // ce que je tape dans l'url
+	public String info() {
+		return "home/info"; // le nom de la vue sous template /home
+	}
+	@RequestMapping("/affichage") //!
+	public String affichage() {
+		return "home/affichage";
+	}
+}
